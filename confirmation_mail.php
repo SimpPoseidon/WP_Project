d7e447a729176ad80c11a1b2072fbadad0d306e3
Kartikey2011yadav <nykay10968@gmail.com>
2022-12-13 13:21:37 +0530

    update

---
+++ confirmation_mail.php
@@ -0,0 +1,12 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+if (isset($_POST['name']) && isset($_POST['Application_no']) && isset($_POST['email'])){
+    $name = $_POST['name'];
+    $email= $_POST['email'];
+    $Application_no = $_POST['Application_no'];
+    $name = $_POST['name'];
+    $name = $_POST['name'];
+}
+
+?>
