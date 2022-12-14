923c3d796dc16bee7a99d5aaf10d483d0cf510b4
Kartikey2011yadav <nykay10968@gmail.com>
2022-12-14 13:31:23 +0530

    c_mail

--- confirmation_mail.php
+++ confirmation_mail.php
@@ -5,8 +5,38 @@ if (isset($_POST['name']) && isset($_POST['Application_no']) && isset($_POST['em
     $name = $_POST['name'];
     $email= $_POST['email'];
     $Application_no = $_POST['Application_no'];
-    $name = $_POST['name'];
-    $name = $_POST['name'];
+
+    require_once "PHPMailer/PHPMailer.php";
+    require_once "PHPMailer/SMTP.php";
+    require_once "PHPMailer/Exception.php";
+
+    $mail = new PHPMailer(true);
+
+   // smtp settings
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "email@gmail.com"; //replace via official email
+    $mail->Password = "email.password"; //replace with password of official email
+    $mail->Port = 587;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+
+    $mail->isHTML(true);
+    $mail->setFrom($email, $name);
+    $mail->addAddress ("email@gmail.com");
+    $mail->Subject = ;
+    $mail->Body = $body;
+
+    $sendSuccesfully = $mail->send();
+    if($sendSuccesfully){
+         $status = "success";
+         $response = "Email is sent!";
+    }
+    else
+        $status = "failed";
+        $response = "Something is wrong: <br>"
+
 }
 
 ?>
