5bf042280736ccf58f9ccca8450ae56cee290c4a
SimpPoseidon <120441879+SimpPoseidon@users.noreply.github.com>
2022-12-28 12:37:19 +0530

    semi semi finale

--- ad.php
+++ ad.php
@@ -1,7 +1,5 @@
 <?php
     include "dbconnect.php";
-    $counter=2;
-    $cls_cnt=0;
 
     $u=$_POST["Username"];
     $p=$_POST["Password"];
@@ -27,55 +25,57 @@
             <center>
             <div id='prev' style='font-family: Lucida Console; font-size:small; '>
             <h3 style='font-size:xx-large;'>Database</h3>
-            <table border='3' cellpadding='7'>
+            <table class='table table-striped table-dark table-bordered table-responsive' border='3' cellpadding='7'>
             <tr>
-            <th align='center'>Application_ID</th>
-            <th align='center'>Name</th>
-            <th align='center'>Fathers_Name</th>
-            <th align='center'>Mothers_Name</th>
-            <th align='center'>PermanentAddress</th>
+            <th align='center'>Application ID</th>
+            <th align='center'>Student`s Name</th>
+            <th align='center'>Father`s Name</th>
+            <th align='center'>Mother`s Name</th>
+            <th align='center'>Postal Address</th>
             <th align='center'>Email</th>
             <th align='center'>Pincode</th>
-            <th align='center'>MobileNo</th>
-            <th align='center'>AltNo</th>
-            <th align='center'>LocalAddress</th>
-            <th align='center'>Program</th>
-            <th align='center'>ClassRollNo</th>
-            <th align='center'>EnrollmentNo</th>
-            <th align='center'>ReceiptNo</th>
-            <th align='center'>Date_Receipt</th>
+            <th align='center'>Mobile No.</th>
+            <th align='center'>Alternate No.</th>
+            <th align='center'>Course Enrolled</th>
+            <th align='center'>Roll No.</th>
+            <th align='center'>Enrollment No.</th>
+            <th align='center'>Account no.</th>
+            <th align='center'>IFSC Code</th>
+            <th align='center'>Passbook</th>
+            <th align='center'>Receipt No.</th>
+            <th align='center'>Date of Receipt</th>
             <th align='center'>Amount</th>
-            <th align='center'>File</th>
-            <th align='center'>Amount_Refunded</th>
-            <th align='center'>Date_of_sub</th>
-            <th align='center'>Time_of_sub</th>
+            <th align='center'>Fees Receipt or Affidavit</th>
+            <th align='center'>Process</th>
+            <th align='center'>Date of Submission</th>
+            <th align='center'>Time of Submission</th>
             </tr>";
             while($row=$display->fetch_assoc())
             {
-                echo"<tr class='cls_$cls_cnt'>
+                echo"<tr>
                 <td align='center'>".$row["Application_ID"]."</td>
                 <td align='center'>".$row["Name"]."</td>
                 <td align='center'>".$row["Fathers_Name"]."</td>
                 <td align='center'>".$row["Mothers_Name"]."</td>
-                <td align='center'>".$row["PermanentAddress"]."</td>
+                <td align='center'>".$row["PostalAddress"]."</td>
                 <td align='center'>".$row["Email"]."</td>
                 <td align='center'>".$row["Pincode"]."</td>
                 <td align='center'>".$row["MobileNo"]."</td>
                 <td align='center'>".$row["AltNo"]."</td>
-                <td align='center'>".$row["LocalAddress"]."</td>
-                <td align='center'>".$row["Program"]."</td>
+                <td align='center'>".$row["CourseEnrolled"]."</td>
                 <td align='center'>".$row["ClassRollNo"]."</td>
                 <td align='center'>".$row["EnrollmentNo"]."</td>
+                <td align='center'>".$row["AccountNo"]."</td>
+                <td align='center'>".$row["IFSC"]."</td>
+                <td align='center'>".$row["Passbook"]."</td>
                 <td align='center'>".$row["ReceiptNo"]."</td>
                 <td align='center'>".$row["Date_Receipt"]."</td>
                 <td align='center'>".$row["Amount"]."</td>
-                <td align='center'>".$row["File"]."</td>
+                <td align='center'>".$row["ReceiptFile"]."</td>
                 <td align='center'>".$row["Amount_Refunded"]."</td>
                 <td align='center'>".$row["Date_of_sub"]."</td>
                 <td align='center'>".$row["Time_of_sub"]."</td>
                 </tr>";
-                $counter++;
-                $cls_cnt=$counter%2;
             }
             echo"</table>
                 </div>
